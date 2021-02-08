f3d5219b4999680aa1f321a04c7565945ff73004
aidatynybekkyzy <tynybekkyzyaida@gmail.om>
2021-02-08 18:40:27 +0600

    added second button

--- app/src/main/java/kg/aidadev/lessongit/MainActivity.java
+++ app/src/main/java/kg/aidadev/lessongit/MainActivity.java
@@ -3,14 +3,17 @@ package kg.aidadev.lessongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView helloTv;
+    //TextView helloTv;
+    Button secondButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        secondButton.findViewById(R.id.second_button);
     }
 }
